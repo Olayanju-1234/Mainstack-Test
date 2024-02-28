454f2e6e86c6293fb74cbb19fd23481c6859d615
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2024-02-28 20:12:19 +0100

    fix : corrected error in server

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import config from "@config/envs";
+
+const { NODE_ENV } = config;
+
+const app = express();
+
+// ** CORS **
+let whitelist: string[] = [
+  "http://localhost:3000",
+];
+
+export const whitelistUrls = whitelist;
+
+if (NODE_ENV !== "production") {
+  whitelist = [...whitelist, "http://localhost:3000"];
+}
+
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback("Not allowed by CORS", false);
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+// ** MORGAN **
+app.use(morgan("dev"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// ** ROUTES **
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).send("Api is running");
+});
+
+// ** Undefined Routes **
+app.use("*", (req: Request, res: Response) => {
+  return res.status(404).json({
+    success: false,
+    message: "API endpoint doesn't exist",
+  });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import "module-alias/register";
+import config from "@config/envs";
+
+const { PORT } = config;
+
+import app from "./app";
+import { connectDB } from "@config/database";
+
+// Connect to the database
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    }
+    );
+}
+);
