ebaf12dcd385f935b92db1ed618a107222f5d67d
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2024-03-02 08:59:20 +0100

    fix : rate limiting

--- src/utils/ratelimit.ts
+++ src/utils/ratelimit.ts
@@ -1,11 +1,11 @@
 import { rateLimit } from 'express-rate-limit';
 
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 3 * 60 * 1000, // 3 minutes
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
     standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-    message: 'Too many requests from this IP, please try again after an hour',
+    message: 'Too many requests from this IP, please try again after 3 mins',
     // store: ... , // Redis, Memcached, etc. See below.
     validate: {xForwardedForHeader: false}, // Disable `X-Forwarded-For` header validation.
 });
